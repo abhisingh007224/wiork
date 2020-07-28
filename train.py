0e50e4f1e69440250872135e991985aaa2b73330
Abhishek Singh 007 <42486179+abhisingh007224@users.noreply.github.com>
2020-07-28 08:23:21 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,169 @@
+import matplotlib
+matplotlib.use("Agg")
+# import the necessary packages
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import img_to_array
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from imutils import paths
+import tensorflow as tf
+import numpy as np
+import argparse
+import random
+import pickle
+import cv2
+import os
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import backend as K
+
+
+class SmallerVGGNet:
+	@staticmethod
+	def build(width, height, depth, classes, finalAct="softmax"):
+		# initialize the model along with the input shape to be
+		# "channels last" and the channels dimension itself
+		model = Sequential()
+		inputShape = (height, width, depth)
+		chanDim = -1
+		# if we are using "channels first", update the input shape
+		# and channels dimension
+		if K.image_data_format() == "channels_first":
+			inputShape = (depth, height, width)
+			chanDim = 1
+		# CONV => RELU => POOL
+		model.add(Conv2D(32, (3, 3), padding="same",
+			input_shape=inputShape))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(MaxPooling2D(pool_size=(3, 3)))
+		model.add(Dropout(0.25))
+		model.add(Conv2D(64, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(Conv2D(64, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(MaxPooling2D(pool_size=(2, 2)))
+		model.add(Dropout(0.25))
+		# (CONV => RELU) * 2 => POOL
+		model.add(Conv2D(128, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(Conv2D(128, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(MaxPooling2D(pool_size=(2, 2)))
+		model.add(Dropout(0.25))
+		model.add(Flatten())
+		model.add(Dense(1024))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization())
+		model.add(Dropout(0.5))
+		# softmax classifier
+		model.add(Dense(classes))
+		model.add(Activation(finalAct))
+		# return the constructed network architecture
+		return model
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+	help="path to input dataset (i.e., directory of images)")
+ap.add_argument("-m", "--model", required=True,
+	help="path to output model")
+ap.add_argument("-l", "--labelbin", required=True,
+	help="path to output label binarizer")
+ap.add_argument("-p", "--plot", type=str, default="plot.png",
+	help="path to output accuracy/loss plot")
+args = vars(ap.parse_args())
+
+EPOCHS = 30
+INIT_LR = 1e-3
+BS = 32
+IMAGE_DIMS = (96, 96, 3)
+# disable eager execution
+tf.compat.v1.disable_eager_execution()
+
+print("[INFO] loading images...")
+imagePaths = sorted(list(paths.list_images(args["dataset"])))
+random.seed(42)
+random.shuffle(imagePaths)
+# initialize the data and labels
+data = []
+labels = []
+
+for imagePath in imagePaths:
+	# load the image, pre-process it, and store it in the data list
+	image = cv2.imread(imagePath)
+	image = cv2.resize(image, (IMAGE_DIMS[1], IMAGE_DIMS[0]))
+	image = img_to_array(image)
+	data.append(image)
+	# extract set of class labels from the image path and update the
+	# labels list
+	l = label = imagePath.split(os.path.sep)[-2].split("_")
+	labels.append(l)
+
+data = np.array(data, dtype="float") / 255.0
+labels = np.array(labels)
+print("[INFO] data matrix: {} images ({:.2f}MB)".format(
+	len(imagePaths), data.nbytes / (1024 * 1000.0)))
+print("[INFO] class labels:")
+mlb = MultiLabelBinarizer()
+labels = mlb.fit_transform(labels)
+# loop over each of the possible class labels and show them
+for (i, label) in enumerate(mlb.classes_):
+	print("{}. {}".format(i + 1, label))
+
+(trainX, testX, trainY, testY) = train_test_split(data,
+	labels, test_size=0.2, random_state=42)
+# construct the image generator for data augmentation
+aug = ImageDataGenerator(rotation_range=25, width_shift_range=0.1,
+	height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
+	horizontal_flip=True, fill_mode="nearest")
+
+print("[INFO] compiling model...")
+model = SmallerVGGNet.build(
+	width=IMAGE_DIMS[1], height=IMAGE_DIMS[0],
+	depth=IMAGE_DIMS[2], classes=len(mlb.classes_),
+	finalAct="sigmoid")
+# initialize the optimizer (SGD is sufficient)
+opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+
+
+model.compile(loss="binary_crossentropy", optimizer=opt,
+	metrics=["accuracy"])
+# train the network
+print("[INFO] training network...")
+H = model.fit(
+	x=aug.flow(trainX, trainY, batch_size=BS),
+	validation_data=(testX, testY),
+	steps_per_epoch=len(trainX) // BS,
+	epochs=EPOCHS, verbose=1)
+
+print("[INFO] serializing network...")
+model.save(args["model"], save_format="h5")
+# save the multi-label binarizer to disk
+print("[INFO] serializing label binarizer...")
+f = open(args["labelbin"], "wb")
+f.write(pickle.dumps(mlb))
+f.close()
+plt.style.use("ggplot")
+plt.figure()
+N = EPOCHS
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="upper left")
+plt.savefig(args["plot"])
